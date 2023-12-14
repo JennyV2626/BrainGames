df857287795148ffa4d827b1fdbf83af4beea144
JennyV2626 <92806857+JennyV2626@users.noreply.github.com>
2023-12-14 14:27:27 -0800

    Added BackToHome Button on Activity 2

--- app/src/main/java/com/example/braingames/MinigamesPage.java
+++ app/src/main/java/com/example/braingames/MinigamesPage.java
@@ -3,12 +3,27 @@ package com.example.braingames;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MinigamesPage extends AppCompatActivity {
 
+    android.widget.Button backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minigames_page);
+
+        backButton = (android.widget.Button) findViewById(R.id.backToHome);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity(){
+        android.content.Intent intent = new android.content.Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }

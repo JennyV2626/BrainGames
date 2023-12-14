e6debfb4b8eccecf3c21c27e3197665b34843e80
JennyV2626 <92806857+JennyV2626@users.noreply.github.com>
2023-12-14 13:22:08 -0800

    Added button functionality

--- app/src/main/java/com/example/braingames/MainActivity.java
+++ app/src/main/java/com/example/braingames/MainActivity.java
@@ -4,28 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textView;
+    android.widget.ImageButton logoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.text);
-
-        ImageButton logo = findViewById(R.id.logo);
-
-        logo.setOnClickListener(new View.OnClickListener()){
+        logoButton = (android.widget.ImageButton) findViewById(R.id.logoButton);
+        logoButton.setOnClickListener(new View.OnClickListener() {
             @Override
-
-        }
-
+            public void onClick(View view) {
+                openMinigamesPage();
+            }
+        });
     }
 
-    
-
+    public void openMinigamesPage(){
+        android.content.Intent intent = new android.content.Intent(this, MinigamesPage.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/example/braingames/MinigamesPage.java
@@ -0,0 +1,14 @@
+package com.example.braingames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MinigamesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minigames_page);
+    }
+}
